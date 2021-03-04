a965fe7189e53e1a49c9d570b1d7fbdd1f5d529f
Iana Delia <ianadelia18@stud.ase.ro>
2021-03-04 09:58:17 +0200

    seminar2 final

--- Seminar2/src/ro/ase/cts/seminar2/Account.java
+++ Seminar2/src/ro/ase/cts/seminar2/Account.java
@@ -1,8 +1,10 @@
 package ro.ase.cts.seminar2;
 
+import ro.ase.cts.seminar2.exceptii.InsufficientFundsException;
+
 public abstract class Account {
 public abstract void deposit(double amount);
-public abstract void withdraw(double amount);
-public abstract void transfer(double amount, Account detination);
+public abstract void withdraw(double amount)  throws InsufficientFundsException;
+public abstract void transfer(double amount, Account detination) throws IllegalTransferException, InsufficientFundsException;
 public abstract double getBalance();
 }
--- Seminar2/src/ro/ase/cts/seminar2/Main.java
+++ Seminar2/src/ro/ase/cts/seminar2/Main.java
@@ -1,12 +1,23 @@
 package ro.ase.cts.seminar2;
 
+import ro.ase.cts.seminar2.exceptii.InsufficientFundsException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//CurrentAccount c=new CurrentAccount(300, "IBAN1");
-		//System.out.println("suma disponibila este:"+ c.getBalance());
+		CurrentAccount c=new CurrentAccount(300, "IBAN1");
 
-		System.out.println("Creditul maxim pentru contul curent este: " + CurrentAccount.MAX_CREDIT);
+		System.out.println("suma disponibila este:"+ c.getBalance());
+        System.out.println("Creditul maxim pentru contul curent este: " + CurrentAccount.MAX_CREDIT);
+	    c.deposit(200);
+	    
+	    System.out.println("suma disponibila este:"+ c.getBalance());
+	    try {
+			c.withdraw(200);
+		} catch (InsufficientFundsException e) {
+			System.err.println(e.getMessage());
+		}
+	    System.out.println("suma disponibila este:"+ c.getBalance());
 	}
 
 }
