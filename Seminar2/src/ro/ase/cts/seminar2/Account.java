4fc8501959a72785197eff6859db875bcb607d7c
Iana Delia <ianadelia18@stud.ase.ro>
2021-03-04 08:23:27 +0200

    before break

---
+++ Seminar2/src/ro/ase/cts/seminar2/Account.java
@@ -0,0 +1,8 @@
+package ro.ase.cts.seminar2;
+
+public abstract class Account {
+public abstract void deposit();
+public abstract void withdraw();
+public abstract void transfer();
+public abstract double getBalance();
+}
---
+++ Seminar2/src/ro/ase/cts/seminar2/CurrentAccount.java
@@ -0,0 +1,34 @@
+package ro.ase.cts.seminar2;
+
+public class CurrentAccount extends BankAccount{
+
+	public static double MAX_CREDIT=5000;
+	public CurrentAccount() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CurrentAccount(double balance, String iban ) {
+		super(balance, iban);
+	
+	}
+
+	@Override
+	public void deposit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transfer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
