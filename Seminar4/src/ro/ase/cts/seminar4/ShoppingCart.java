689eb980e583ca05368439b3097ee63b54401e7c
ianab <ianab@DESKTOP-D9RQB22>
2021-03-18 09:27:00 +0200

    Intial seminar4

--- Seminar4/src/ro/ase/cts/seminar4/ShoppingCart.java
+++ Seminar4/src/ro/ase/cts/seminar4/ShoppingCart.java
@@ -2,4 +2,16 @@ package ro.ase.cts.seminar4;
 
 public class ShoppingCart {
 
+	private static ShoppingCart instance=null;
+	
+	private ShoppingCart() {}
+
+    public static ShoppingCart getInstance() {
+    	if(instance==null) {
+    		instance=new ShoppingCart();
+    	}
+		
+		return instance;
+	}
+	
 }
